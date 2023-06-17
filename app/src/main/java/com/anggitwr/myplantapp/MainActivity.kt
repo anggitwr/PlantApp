b158952397f6421dde1ef1506a78af8d5fb01276
anggitwr <agtwahyu23@gmail.com>
2023-06-17 09:32:51 +0700

    add model

--- app/src/main/java/com/anggitwr/myplantapp/MainActivity.kt
+++ app/src/main/java/com/anggitwr/myplantapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.anggitwr.myplantapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.anggitwr.myplantapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun startCamera(){
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 1)
+        } else {
+            //Request camera permission if we don't have it.
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+        }
+    }
+
+    private fun startGallery(){
+        val intent = Intent()
+
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 100)
+
+    }
+
+    private fun classification(){
+
     }
 }
