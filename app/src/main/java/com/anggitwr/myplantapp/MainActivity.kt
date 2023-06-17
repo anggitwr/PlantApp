a1565eff076655a3344a5814ea6b9373901b1d2d
anggitwr <agtwahyu23@gmail.com>
2023-06-17 12:16:42 +0700

    update tflite, add permission, update mainActivity

--- app/src/main/java/com/anggitwr/myplantapp/MainActivity.kt
+++ app/src/main/java/com/anggitwr/myplantapp/MainActivity.kt
@@ -3,14 +3,26 @@ package com.anggitwr.myplantapp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import com.anggitwr.myplantapp.databinding.ActivityMainBinding
+import com.anggitwr.myplantapp.ml.ConvertedModelav472Lite
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var bitmap: Bitmap
+
+    var imageSize = 224
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -39,7 +51,151 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun classification(){
+    private fun classification(image : Bitmap?){
+        try {
+            val model = ConvertedModelav472Lite.newInstance(applicationContext)
 
+            // Creates inputs for reference.
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+
+            val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+
+            // get 1D array of 200 * 200 pixels in image
+            val intValues = IntArray(imageSize * imageSize)
+            image!!.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            var pixel = 0
+            for (i in 0 until imageSize) {
+                for (j in 0 until imageSize) {
+                    val `val` = intValues[pixel++] // RGB
+                    byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            // Runs model inference and gets result.
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+            val confidences = outputFeature0.floatArray
+
+            // find the index of the class with the biggest confidence.
+            var maxPos = 0
+            var minConfidence = 0.3111f
+            var maxConfidence = 0f
+
+            val classes = arrayOf(
+                "Jambu Biji",
+                "Kari",
+                "Kemangi",
+                "Kunyit",
+                "Mint",
+                "Pepaya",
+                "Sirih",
+                "Sirsak",
+                "Lidah Buaya",
+                "Teh Hijau",
+            )
+
+            for (i in confidences.indices) {
+
+                if (confidences[i] > minConfidence) {
+//                    maxConfidence  = confidences[i]
+                    maxPos = i
+
+
+//                    val classes = arrayOf(
+//                        "Jambu Biji",
+//                        "Kari",
+//                        "Kemangi",
+//                        "Kunyit",
+//                        "Mint",
+//                        "Pepaya",
+//                        "Sirih",
+//                        "Sirsak",
+//                        "Lidah Buaya",
+//                        "Teh Hijau",
+//                    )
+////            showLoading(false)
+                } else if (confidences[i] < minConfidence) {
+                    binding.tvResultkurang.setText(R.string.hasil_kurang)
+                }
+                return
+            }
+
+            binding.tvResult.text = classes[maxPos]
+            var s = ""
+            for (i in 0 until classes.size) {
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100.0f)
+            }
+            binding.tvConfidence.setText(s)
+
+            if (classes[maxPos] == "Jambu Biji") {
+                binding.tvDetail.setText(R.string.perubahan_jambubiji_detail)
+            }
+            if (classes[maxPos] == "kari") {
+                binding.tvDetail.setText(R.string.perubahan_kari_detail)
+            }
+            if (classes[maxPos] == "Kemangi") {
+                binding.tvDetail.setText(R.string.perubahan_kemangi_detail)
+            }
+            if (classes[maxPos] == "Kunyit") {
+                binding.tvDetail.setText(R.string.perubahan_kunyit_detail)
+            }
+            if (classes[maxPos] == "Mint") {
+                binding.tvDetail.setText(R.string.perubahan_mint_detail)
+            }
+            if (classes[maxPos] == "Pepaya") {
+                binding.tvDetail.setText(R.string.perubahan_pepaya_detail)
+            }
+            if (classes[maxPos] == "Sirih") {
+                binding.tvDetail.setText(R.string.perubahan_sirih_detail)
+            }
+            if (classes[maxPos] == "Sirsak") {
+                binding.tvDetail.setText(R.string.perubahan_sirsak_detail)
+            }
+            if (classes[maxPos] == "Lidahbuaya") {
+                binding.tvDetail.setText(R.string.perubahan_lidahbuaya_detail)
+            }
+            if (classes[maxPos] == "Teh Hijau") {
+                binding.tvDetail.setText(R.string.perubahan_tehhijau_detail)
+            }
+
+            // Releases model resources if no longer used.
+            model.close()
+        } catch (e: Exception){
+            binding.tvDetail.setText(R.string.not_identification)}
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            var image = data!!.extras!!["data"] as Bitmap?
+            val dimension = Math.min(image!!.width, image.height)
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+            binding.imageView.setImageBitmap(image)
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+            binding.btnPrediction.setOnClickListener { classification(image) }
+        }
+
+        else if (requestCode == 100){
+            var uri = data?.data
+            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,uri)
+            val dimension = Math.min(bitmap.width, bitmap.height)
+            bitmap = ThumbnailUtils.extractThumbnail(bitmap, dimension, dimension)
+            binding.imageView.setImageBitmap(bitmap)
+            bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false)
+            binding.btnPrediction.setOnClickListener { classification(bitmap) }
+//            onSupportNavigateUp()
+        }
+        else {
+            onBackPressed()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
