1bb81faa7d3f3da812287657a2273a546b2eb88d
anggittwr <agtwahyu23@gmail.com>
2023-06-30 11:13:37 +0700

    add onBoard and some update

---
+++ app/src/main/java/com/anggitwr/myplantapp/OnboardingActivity.kt
@@ -0,0 +1,31 @@
+package com.anggitwr.myplantapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.anggitwr.myplantapp.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        action()
+    }
+
+    private fun action(){
+        binding.btnOnb1Lewati.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnOnb1Lanjut.setOnClickListener {
+            val intent = Intent(this, OnboardingActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
